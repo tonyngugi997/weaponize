7e65986f9c50c1d2b4cb7b74940fc1b31c8cedfe
tonyngugi997 <tonyngugi997@gmail.com>
2026-04-04 14:33:45 +0300

    Initial  lua commit commit with basic settings

---
+++ init.lua
@@ -0,0 +1,31 @@
+--##############################################
+--weaponize configuration script
+--###############################################
+-- we start with basic default settings
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.updatetime = 50
+vim.opt.timeoutlen = 300
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--##############################################
+
