3678e0bcf3f2dbdc25f2b2e9edc9896d4b59679d
tonyngugi997 <tonyngugi997@gmail.com>
2026-04-04 14:07:56 +0300

    feat(completion): add nvim-cmp with LSP, buffer, path sources, snippets, and icons

--- init.lua
+++ init.lua
@@ -161,3 +161,79 @@ require("lazy").setup({
         end,
     }
 
+
+    
+    -- =========================== AUTOCOMPLETION (Bordered, Icons, Snippets) ===========================
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "L3MON4D4/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+            "onsails/lspkind.nvim",
+        },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+            require("luasnip.loaders.from_vscode").lazy_load()
+            require("lspkind").init()
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered({
+                        border = "rounded",
+                        winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+                    }),
+                    documentation = cmp.config.window.bordered({
+                        border = "rounded",
+                        winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+                    }),
+                },
+                formatting = {
+                    format = require("lspkind").cmp_format({
+                        mode = "symbol_text",
+                        maxwidth = 50,
+                        ellipsis_char = "...",
+                        before = function(entry, vim_item)
+                            vim_item.menu = "[" .. (entry.source.name or "?") .. "]"
+                            return vim_item
+                        end,
+                    }),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-n>"] = cmp.mapping.select_next_item(),
+                    ["<C-p>"] = cmp.mapping.select_prev_item(),
+                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+                experimental = { ghost_text = true },
+            })
+        end,
+    },
+    "L3MON4D4/LuaSnip",
+    "rafamadriz/friendly-snippets",
+    "onsails/lspkind.nvim",
+
