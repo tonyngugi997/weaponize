5bf75f7e6574807d2c6a3c8d8578f20df411f143
tonyngugi997 <tonyngugi997@gmail.com>
2026-04-06 08:08:16 +0300

    feat(statusline): add lualine with catppuccin theme and custom sections

--- init.lua
+++ init.lua
@@ -318,3 +318,22 @@ require("lazy").setup({
             })
         end,
     },
+
+        -- =========================== STATUSLINE (Lualine) ===========================
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup({
+                options = { theme = "catppuccin", component_separators = "", section_separators = "" },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch" },
+                    lualine_c = { "filename", "diagnostics" },
+                    lualine_x = { "encoding", "fileformat", "filetype" },
+                    lualine_y = { "progress" },
+                    lualine_z = { "location" },
+                },
+            })
+        end,
+    },
